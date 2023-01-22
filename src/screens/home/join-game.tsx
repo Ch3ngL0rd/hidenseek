75a18a54158b5f3f757c6d4cf75a645c2302fb35
Muffin-Man2 <85885968+Muffin-Man2@users.noreply.github.com>
2023-01-22 16:21:14 +0800

    Added join game functionality

--- src/screens/home/join-game.tsx
+++ src/screens/home/join-game.tsx
@@ -1,11 +1,50 @@
 import { NavigationProp } from '@react-navigation/native';
 import React from 'react';
-import { SafeAreaView, Text } from 'react-native';
+import { Button, SafeAreaView, Text, TextInput } from 'react-native';
+import { supabase } from '../../settings/supabase';
 
 export default function JoinGame({ navigation }: { navigation: NavigationProp<any> }) {
+    const [gameCode, setGameCode] = React.useState<string>('');
+    const [loading, setLoading] = React.useState<boolean>(false);
+    const [message, setMessage] = React.useState<string>("");
+
+    const joinGame = async () => {
+        setLoading(true);
+        // Make sure that game exists
+        let { data: games, error } = await supabase
+            .from('games')
+            .select("*")
+            .eq('code', gameCode)
+        console.log(games);
+        if (error) {
+            console.log(error);
+            setMessage("Error joining game")
+            setLoading(false);
+            return;
+        } else if (!games) {
+            setMessage("Game does not exist");
+            setLoading(false);
+            return;
+        } else if (games.length === 0) {
+            setMessage("Game does not exist");
+            setLoading(false);
+            return;
+        }
+
+        navigation.navigate("Ready", { game_id: gameCode })
+        setLoading(false);
+    }
+
     return (
         <SafeAreaView className="flex justify-center items-center flex-col">
-            <Text>Tutorial</Text>
+            <Text>Game Code</Text>
+            <TextInput
+                style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => setGameCode(text.toUpperCase())}
+                value={gameCode}
+                placeholder="Enter Game Code"
+            />
+            <Button title="Join Game" onPress={joinGame} />
         </SafeAreaView>
     );
 }   
